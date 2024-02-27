373c39346eef9b2b290d4cb99899d234d94f1b6b
hhxjqm <hhxjqm@gamil.com>
2024-02-26 20:39:04 -0500

    updated

--- MyAutoPano/Phase2/Code/tttt.py
+++ MyAutoPano/Phase2/Code/tttt.py
@@ -1,13 +1,39 @@
 import torchvision.transforms as transforms
 from PIL import Image
+import numpy as np
+import cv2
 
-# 假设您已经有了一个图像的路径
-image_path = r'..\Data\Train\500.jpg'
+image_path = r'..\Data\Train\2.jpg'
 
-# 加载图像并转换为Tensor
 image = Image.open(image_path)
 transform = transforms.ToTensor()
 tensor_image = transform(image)
 
-# 打印图像的大小
-print(tensor_image.size())  # 或者 print(tensor_image.shape)
+print(tensor_image.size())
+
+def apply_homography_and_crop_patch(I1, max_warp=32):
+    h, w = I1.shape[:2]
+    perturbations = np.float32([
+        [np.random.uniform(-max_warp, max_warp), np.random.uniform(-max_warp, max_warp)],
+        [np.random.uniform(-max_warp, max_warp), np.random.uniform(-max_warp, max_warp)],
+        [np.random.uniform(-max_warp, max_warp), np.random.uniform(-max_warp, max_warp)],
+        [np.random.uniform(-max_warp, max_warp), np.random.uniform(-max_warp, max_warp)],
+    ])
+    
+    src_points = np.float32([[0, 0], [w, 0], [w, h], [0, h]])
+    dst_points = src_points + perturbations
+    
+    H = cv2.getPerspectiveTransform(src_points, dst_points)
+    
+    I1_warped = cv2.warpPerspective(I1, H, (w, h))
+    
+    return I1_warped, H
+
+I1 = cv2.imread(image_path).astype(np.float32)
+I1_warped, H = apply_homography_and_crop_patch(I1)
+
+
+print("Homography Matrix H:\n", H)
+
+output_image_path = 'warped_1.jpg'
+cv2.imwrite(output_image_path, I1_warped)
